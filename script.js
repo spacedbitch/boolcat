009f50911ae1e7cc6880fea7a17b77ce095dbf57
slusheru5 <sueniques@hotmail.com>
2023-04-20 23:52:32 -0400

    content - needs layout fix

--- script.js
+++ script.js
@@ -1,138 +1,127 @@
-const cat = document.getElementById('cat');
-const gameCanvas = document.getElementById('game-canvas');
-const gameStartScreen = document.getElementById('game-start-screen');
 const gameStartButton = document.getElementById('game-start-button');
+const gameCanvas = document.getElementById('game-canvas');
+const cat = document.getElementById('cat');
 const mouseJail = document.getElementById('mouse-jail');
-const starContainer = document.getElementById('star-container');
 const winBanner = document.getElementById('win-banner');
 const restartGameButton = document.getElementById('restart-game-button');
+const starContainer = document.getElementById('star-container');
 
-cat.style.left = '50%';
-cat.style.top = '50%';
+gameStartButton.addEventListener('click', startGame);
+restartGameButton.addEventListener('click', restartGame);
 
-let catVelocity = { x: 0, y: 0 };
-let mouseCount = 0;
-let starCount = 0;
-let mice = [];
+function startGame() {
+  gameStartButton.parentElement.classList.add('hidden');
+  gameCanvas.classList.remove('hidden'); // Changed this line
+  winBanner.classList.add('hidden'); // Changed this line
+  gameCanvas.classList.add('hidden'); 
+  
+  
+  cat.style.left = '375px';
+  cat.style.top = '275px';
+  spawnMouse();
+}
 
-function generateMouse() {
+
+function restartGame() {
+  winBanner.classList.add('hidden');
+
+  startGame();
+}
+
+function updateStars(earnedStars) {
+  starContainer.innerHTML = '';
+
+  for (let i = 0; i < earnedStars; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = 8 + (25 * i) + 'px';
+    star.style.top = '8px';
+    starContainer.appendChild(star);
+  }
+}
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+
+  let currentTop = parseInt(cat.style.top);
+  let currentLeft = parseInt(cat.style.left);
+
+  let moveUp = key === 'ArrowUp' || key === 'w';
+  let moveDown = key === 'ArrowDown' || key === 's';
+  let moveLeft = key === 'ArrowLeft' || key === 'a';
+  let moveRight = key === 'ArrowRight' || key === 'd';
+
+  if (moveUp || moveDown || moveLeft || moveRight) {
+    event.preventDefault(); // Prevent scrolling
+  }
+
+  if (moveUp) {
+    currentTop -= 25;
+  }
+  if (moveDown) {
+    currentTop += 25;
+  }
+  if (moveLeft) {
+    currentLeft -= 25;
+  }
+  if (moveRight) {
+    currentLeft += 25;
+  }
+
+  cat.style.top = Math.max(0, Math.min(575, currentTop)) + 'px';
+  cat.style.left = Math.max(0, Math.min(775, currentLeft)) + 'px';
+
+  checkForCollisions();
+});
+function addToMouseJail() {
+  const miceInJail = mouseJail.childElementCount;
+  if (miceInJail < 49) {
+    const newMouse = document.createElement('div');
+    newMouse.classList.add('mouse');
+    newMouse.style.left = 8 + (25 * (miceInJail % 10)) + 'px';
+    newMouse.style.top = 8 + (25 * Math.floor(miceInJail / 10)) + 'px';
+    mouseJail.appendChild(newMouse);
+    updateStars(miceInJail + 1);
+  } else {
+    winGame();
+  }
+}
+
+function updateStars(miceInJail) {
+  const starsEarned = Math.floor(miceInJail / 10);
+  starContainer.innerHTML = '';
+  for (let i = 0; i < starsEarned; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    starContainer.appendChild(star);
+  }
+
+  if (starsEarned === 5) {
+    winGame();
+  }
+}
+function spawnMouse() {
   const mouse = document.createElement('div');
   mouse.classList.add('mouse');
-  mouse.style.left = `${Math.random() * (gameCanvas.clientWidth - mouse.clientWidth)}px`;
-  mouse.style.top = `${Math.random() * (gameCanvas.clientHeight - mouse.clientHeight)}px`;
+  mouse.style.left = Math.floor(Math.random() * 31) * 25 + 'px';
+  mouse.style.top = Math.floor(Math.random() * 23) * 25 + 'px';
   gameCanvas.appendChild(mouse);
-  return mouse;
 }
 
-function moveCat() {
-  const newX = parseFloat(cat.style.left) + catVelocity.x;
-  const newY = parseFloat(cat.style.top) + catVelocity.y;
-  if (newX >= 0 && newX <= gameCanvas.clientWidth - cat.clientWidth) {
-    cat.style.left = newX + 'px';
-  }
-  if (newY >= 0 && newY <= gameCanvas.clientHeight - cat.clientHeight) {
-    cat.style.top = newY + 'px';
-  }
-}
-
-function handleKeyDown(event) {
-  const key = event.key;
-
-  if (key === 'ArrowUp') {
-    catVelocity.y = -5;
-  } else if (key === 'ArrowDown') {
-    catVelocity.y = 5;
-  } else if (key === 'ArrowLeft') {
-    catVelocity.x = -5;
-  } else if (key === 'ArrowRight') {
-    catVelocity.x = 5;
-  }
-}
-
-function handleKeyUp(event) {
-  const key = event.key;
-
-  if (key === 'ArrowUp' || key === 'ArrowDown') {
-    catVelocity.y = 0;
-  } else if (key === 'ArrowLeft' || key === 'ArrowRight') {
-    catVelocity.x = 0;
-  }
-}
-
-function checkMouseCollision(mouse) {
+function checkForCollisions() {
   const catRect = cat.getBoundingClientRect();
-  const mouseRect = mouse.getBoundingClientRect();
-  return !(catRect.right < mouseRect.left ||
-           catRect.left > mouseRect.right ||
-           catRect.bottom < mouseRect.top ||
-           catRect.top > mouseRect.bottom);
-}
-
-function catchMouse(mouse) {
-  gameCanvas.removeChild(mouse);
-  mice = mice.filter(m => m !== mouse);
-  mouseCount++;
-
-  if (mouseCount % 10 === 0) {
-    starCount++;
-    addStar();
-  }
-
-  if (starCount === 5) {
-    winGame();
-  } else {
-    mice.push(generateMouse());
-  }
-}
-
-function addStar() {
-  const star = document.createElement('div');
-  star.classList.add('star');
-  starContainer.appendChild(star);
+  const mice = document.querySelectorAll('.mouse');
+  mice.forEach((mouse) => {
+    const mouseRect = mouse.getBoundingClientRect();
+    if (catRect.left === mouseRect.left && catRect.top === mouseRect.top) {
+      mouse.remove();
+      addToMouseJail();
+      spawnMouse();
+    }
+  });
 }
 
 function winGame() {
   gameCanvas.style.display = 'none';
   winBanner.style.display = 'block';
 }
-
-function startGame() {
-  gameStartScreen.style.display = 'none';
-  gameCanvas.style.display = 'block'; // Add this line
-  for (let i = 0; i < 5; i++) {
-    mice.push(generateMouse());
-  }
-}
-
-function restartGame() {
-  winBanner.style.display = 'none';
-  starContainer.innerHTML = '';
-  mice.forEach(mouse => gameCanvas.removeChild(mouse));
-  mice = [];
-  mouseCount = 0;
-  starCount = 0;
-  cat.style.left = '50%';
-  cat.style.top = '50%';
-  gameCanvas.style.display = 'block';
-  for (let i = 0; i < 5; i++) {
-    mice.push(generateMouse());
-  }
-}
-
-gameStartButton.addEventListener('click', startGame);
-restartGameButton.addEventListener('click', restartGame);
-document.addEventListener('keydown', handleKeyDown);
-document.addEventListener('keyup', handleKeyUp);
-
-function gameLoop() {
-  moveCat();
-  mice.forEach(mouse => {
-    if (checkMouseCollision(mouse)) {
-      catchMouse(mouse);
-    }
-  });
-  requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
-
